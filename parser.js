4dd1d9a9188f8becea7acff7a8aaf8c8682a2869
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-26 08:39:15 +0300

    Refactor code to make it reusable

--- parser.js
+++ parser.js
@@ -1,16 +1,22 @@
 const nightmare = require("nightmare")();
 
+const args = process.argv.slice(2);
+const url = args[0];
+const minPrice = args[1];
+
 checkPrice();
 
 async function checkPrice() {
   const priceString = await nightmare
-    .goto(
-      "https://www.amazon.com/Samsung-T5-Portable-SSD-MU-PA2T0B/dp/B073H4GPLQ"
-    )
+    .goto(url)
     .wait("#priceblock_ourprice")
     .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
     .end();
 
   const priceNumber = parseFloat(priceString.replace("$", ""));
-  console.log(priceNumber);
+  if (priceNumber < minPrice) {
+    console.log("It's cheap");
+  } else {
+    console.log("It's expensive");
+  }
 }
